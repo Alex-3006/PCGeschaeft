f64d09b33a055b99a284bace65a8cfd881062aa3
Alex-3006 <alec.nero.com@gmail.com>
2021-07-19 17:26:10 +0200

    restored to stable point

--- next.config.js
+++ next.config.js
@@ -1,27 +1,16 @@
 module.exports = {
-  reactStrictMode: true,
-  // other next config
-  i18n: {
-    locales: ["en", "ms"],
-    defaultLocale: "en",
+  future: {
+    webpack5: false,
   },
-  env: {
-    SPREADSHEET_ID: "1vuXt8sQq7SkmvTp8lIpAPJAA2hSlK6LFY21-fm4E7pM",
-    SHEET_ID: 0,
-    CLIENT_EMAIL: "alexschool@avian-catwalk-319412.iam.gserviceaccount.com",
-    PRIVATE_KEY: "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCdX5MmYadlIiCH\nEEZyECR+BSoGWg6KUh5qLxF1zY18GenYiHNQ1A1KEF+MmnaHH5IXpSnJz8D7u0DH\noZM4kBcLVsctJE2Ot9aDpRPX5zA352CYoiyzMw1ccoI6k6HZf763TrhyQvdbpBF5\nKYMxIO1K/UqNnEh+kHmrpdFzCpRDFvRhnKGo8igMu67wz2jhc/udOjT5JO6y4s4w\n0R/Hy8kjMaE/cCW8jrjg7+NcuaAw6E5lDa+Vk1P/tIEtfeAiBM0HC/YQfBttZVnd\nAYhghmARjcSUB1yPQ8KqwSRwUcHjMmVkSHW71QuLgbJzcfhZ7KqHES3khtL2LdhN\ncHQUIPyhAgMBAAECggEARNXbTXyFdc0ZoTpTNe0tqRmlKiYWGhaVSyjPYFHKwy+q\ng5Ybqo02SuEiiiO+GJiMlF8gCjAfX3ymdyuLc8BC9Ci+8lXgvr5WzDpWlaeABdPl\nAp5dIvz/qSJFKE4PC02/I/sA3qm//IulQPQReFnIsXZdN9fWNdYIAjaDiOhltXoT\nSfL1ka6Ny9u2ZEBV1tbIlyoPui4u8uNj+YbDkDz5quBR1tlmxeVB+ouQVsl1a3eP\nl0s5TbwOcKji0OuwPxIa2c1I4ifLOGhUVP2qFUriW/IDGffWyoy46oVjaPeOrc6n\ngy9iFvTTYZpNm3wXz22I5w7Qqe2iac4+xVgz5rSsiQKBgQDL6dSdWy7S8/nLZfJ5\nENDKRuXkWok24m3UomPVJkpayy+3J+9xipVLuuV9Eka1brvDrOo6KyUsz+EXsJX3\nt0XGx5GVVwajXWx/wwm7WVUBExdu7rnRKl4wFxCSW7R8rQTADUwKoqeL39ZyVLBF\no7oaXPPvMkPY1+QYAxw6F2jvMwKBgQDFkm1y1spkYcbsYkbQtW4GpnKLoTd9lAfR\nvHvI6o1BeoPK17pzlLDuH6+tH7o70h1wooBv41882H3894jApuFxagDq79toF8yW\nCik/gf7hqCMlp1k3/4plxU32GSkEXUyxs3GcLVEOhKC8yn3eZ1j6nC0kwXVlLnJB\nJHSLKCI02wKBgHeRpH+kwR8nuti+7eB5sMR7sEAnqoAJY/ekRT/uVPlKXa9bdncw\nmPywGfMt7VJkHSvPUAVN2V2eg9HdVQLNb9fwm/hi8FyI4p9EIYe/CS45IKuGxmTV\nICe4zpnTtlDJL5jh8y3lVLObTGb8K+SZ7f2ZddxTsB8N8Qz3fey+X2SrAoGAK+S8\nLCdMD8EpUJsE4JluOEwCZsMKcPvSQOZ/GcQCXem6HfaPld4S8NRZUz0ZQvELWVrA\n7hq1re7rDqhv2V05+Nh0G73uINIowazAPTvERJ32MQdc6vP662FKUMKGOlIAF/ql\n4uyudWtByq+yBxcuzX6lCOVyD2iiOZa/zaq87sECgYBRNyuzm+yhzmIFyQVUpa8L\nSVGL1yDvQBALpFrSXm1hUhTgVUFCsNm43hAVUUxIQyC4AFa2sItFnEqV1zkiZUEF\nras3oK3oNpQMgXzAwrcD+1TnjikRJ+lUEKi1cFLqK8+C+8nShOeWI9NnNL3cGtB/\n9uVp59mkJl4H9PptLdeTGA==\n-----END PRIVATE KEY-----\n",
-  },
-  // webpack: (config, options) => {
-  //   config.node = {
-  //     // Some libraries import Node modules but don't use them in the browser.
-  //     // Tell Webpack to provide empty mocks for them so importing them works.
-  //     ...config.node,
-  //     fs: 'empty',
-  //     child_process: 'empty',
-  //     net: 'empty',
-  //     tls: 'empty',
-  //   }
 
-    // return config
-  // },
+  webpack: (config) => {
+    config.resolve.fallback = { 
+      fs: false,
+      child_process : false,
+      net : false,
+      tls: false,
+    };
+
+    return config;
+  },
 };
